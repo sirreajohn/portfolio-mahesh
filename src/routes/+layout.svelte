2c3804ffacfb9cd32e2c5639765c7e19ef178eaf
mahesh <mahesh.patapali@gmail.com>
2024-10-06 17:43:23 +0530

    fixing deployment_err

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,7 +29,7 @@
 </main>
 <div class="background-gradient absolute inset-0 -z-50" />
 <div
-	class="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('noisetexture.jpg')] opacity-20 mix-blend-soft-light"
+	class="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"
 />
 <Footer settings={data.settings} />
 <PrismicPreview {repositoryName} />
